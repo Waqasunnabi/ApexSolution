93e8b0fa6f6210be50114e5afe7310e1e94b78e1
Waqasunnabi <119784909+Waqasunnabi@users.noreply.github.com>
2023-04-24 03:25:16 +0000

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,22 +6,20 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-        <div className="py-2">
+        <div className="py-1">
 
     </div>
-
     <div>
-        <nav className="bg-slate-400 text-white flex justify-between">
+        <nav className="bg-black text-white flex justify-between">
             <div className="flex" >
-                <Image src="/images/Logo.png" className="h-20 cursor-pointer" alt="me" width="64" height="64" />
-                <h1 className="py-6 text-2xl">Apex Solution</h1>  
+                <h1 className="cursor-pointer px-6 py-6 text-2xl">Apex Solution</h1>  
             </div>
             <ul className="px-10 py-7 flex space-x-3 justify-end">
 
-                <li className="cursor-pointer">Home</li>
-                <li className="cursor-pointer">Service</li>
-                <li className="cursor-pointer">About</li>
-                <li className="cursor-pointer">Contact us</li>
+                <li className="cursor-pointer hover:text-red-500">Home</li>
+                <li className="cursor-pointer hover:text-red-500">Service</li>
+                <li className="cursor-pointer hover:text-red-500">About</li>
+                <li className="cursor-pointer hover:text-red-500">Contact us</li>
 
             </ul>
         </nav>

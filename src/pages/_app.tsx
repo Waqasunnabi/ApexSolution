a7121fdddea83a69be423465acc30359373119cc
Waqasunnabi <119784909+Waqasunnabi@users.noreply.github.com>
2023-04-25 01:08:33 +0000

    adada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ReactNode } from 'react';
 import Layout from '/workspaces/ApexSolution/components/Layout';
+
+type Props = {
+  children: ReactNode;
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
+
 export default MyApp;

190aa6694f220637f73523190f52fe104f06c10f
Waqasunnabi <119784909+Waqasunnabi@users.noreply.github.com>
2023-04-25 01:21:19 +0000

    asdasd

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react";
+import { PropsWithChildren } from 'react';
 import Navbar from "./navbar";
 
 const Layout = ({ children }: PropsWithChildren) => {
